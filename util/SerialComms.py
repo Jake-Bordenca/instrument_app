c45a88c038019664f6b66413106d2ad770194272
Jake-Bordenca <jake.bordenca@stonybrook.edu>
2025-11-18 13:28:03 -0500

    Update SerialComms.py
    replaced previous w/ Chris's async

--- util/SerialComms.py
+++ util/SerialComms.py
@@ -3,12 +3,14 @@ import time
 
 class SerialComms():
     def __init__(self, instrument="Compact", port = 'COM3', baudrate=115200, timeout=10):
-
         self.instrument = instrument
         self.ser = serial.Serial(port = port, baudrate=baudrate, timeout=timeout)
+        self.queue = {}
     
+
     def close(self):
         self.ser.close()
+
 
     def getMessageCompact(self, message):
         message_bytes = bytes(message, 'ascii')
@@ -16,48 +18,65 @@ class SerialComms():
         message = message + "@" + checksum[2:] + "\r"
         return message
 
+
     def sendCompact(self, message):
         message_bytes = bytes(message, 'ascii')
-        message_list = message[0:-6].strip('\r').split(';')
+        message_list = message.strip('\r').split(';')
         checksum = str(hex(self.crc16(message_bytes, 0, len(message_bytes)))).upper()
         message = message + "@" + checksum[2:] + "\r"
         message_bytes = bytes(message, 'ascii')
         self.ser.write(message_bytes)
-        if 'HVC_' in message:
-            time.sleep(.2)
-        else:
-            time.sleep(.015)
-        if self.ser.in_waiting > 0:
-            data = self.ser.read(self.ser.in_waiting)
-            data = data.replace(b'\x00',b'').replace(b'\x06', b'').strip(b'\r').split(b'\r')
-            data = [d.decode('ascii') for d in data]
-            results = []
-            responses = []
-            for m, d in zip(message_list, data):
-                if m in d:
-                    response, checksum = d.split("@")
-                    checkchecksum = str(hex(self.crc16(bytes(response, 'ascii'), 0, len(response))))[2:].upper()
-                    while len(checkchecksum) < 4:
-                        checkchecksum = "0" + checkchecksum
-                    if checksum == checkchecksum:
-                        if "?" in response:
-                            results.append(response.split("?")[-1])
-                            responses.append(response)
-                        elif "=" in response:
-                            results.append(response.split("=")[-1])
-                            responses.append(response)
-                    else:
-                        print("Checksum wrong")
-                        return None
-        else:
-            print("No response to command")
-            return None
-        if len(results) > 0:
-            return results, responses
-        else:
-            print("Response empty")
-            return None, None
+        for m in message_list:
+            if '=' in m:
+                m = m.split('=')[0] + '='
+            #print("add", m)
+            self.queue[m] = time.perf_counter()
+
+
+    def readCompact(self):
+        messages, values, responses = [], [], []
+        while self.ser.in_waiting > 0:
+            data = self.ser.read_until(b'\r')
+            data = data.replace(b'\x00',b'').replace(b'\x06', b'').strip(b'\r').decode('ascii')
+            if "@" not in data:
+                continue
+            response, checksum = data.split("@")
+            check_checksum = str(hex(self.crc16(bytes(response, 'ascii'), 0, len(response))))[2:].upper()
+            while len(check_checksum) < 4:
+                check_checksum = "0" + check_checksum
+            if checksum == check_checksum:
+                if "?" in response:
+                    message, value = response.split("?")
+                    message = message + "?"
+                    messages.append(message)
+                    values.append(value)
+                    responses.append(response)
+                elif "=" in response:
+                    message, value = response.split("=")
+                    message = message + "="
+                    messages.append(message)
+                    values.append(value)
+                    responses.append(response)
+                else:
+                    print("Unexpected response format received:", response)
+                    break
+            else:
+                print("Checksum wrong")
+                break
+
+            del self.queue[message]
+
+        return messages, values, responses
         
+    
+    def purgeQueue(self):
+        current_time = time.perf_counter()
+        for message, send_time in self.queue.items():
+            if current_time - send_time > 1000:
+                print('Purging', message)
+                del self.queue[message]
+
+
     @staticmethod
     def crc16(data : bytearray, offset , length):
         if data is None or offset < 0 or offset > len(data)- 1 and offset+length > len(data):
@@ -72,12 +91,11 @@ class SerialComms():
                     crc = crc << 1
         return crc & 0xFFFF
 
+
 if __name__ == '__main__':
     ser = SerialComms()
-    #message = ser.getMessageCompact('QP_1:RFA_?')
+    
     message = ser.getMessageCompact('TP_1:MOSW?;TP_1:ROTR?;TP_1:POWR?')
     print(ser.sendCompact(message))
 
-
-    
     ser.close()
